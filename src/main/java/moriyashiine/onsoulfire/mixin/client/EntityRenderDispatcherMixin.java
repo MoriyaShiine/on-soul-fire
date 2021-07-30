0729b20f0a52278ea099b5496459e48ec40cf162
MoriyaShiine <moriyashiine@gmail.com>
2021-07-30 10:46:57 -0500

    use ModifyVariable instead of Redirect

--- src/main/java/moriyashiine/onsoulfire/mixin/client/EntityRenderDispatcherMixin.java
+++ src/main/java/moriyashiine/onsoulfire/mixin/client/EntityRenderDispatcherMixin.java
@@ -13,7 +13,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Environment(EnvType.CLIENT)
 @Mixin(EntityRenderDispatcher.class)
@@ -21,19 +21,19 @@ public class EntityRenderDispatcherMixin {
 	private static final SpriteIdentifier SOUL_FIRE_0 = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier("block/soul_fire_0"));
 	private static final SpriteIdentifier SOUL_FIRE_1 = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier("block/soul_fire_1"));
 	
-	@Redirect(method = "renderFire", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/SpriteIdentifier;getSprite()Lnet/minecraft/client/texture/Sprite;", ordinal = 0))
-	private Sprite getSprite0(SpriteIdentifier spriteIdentifier, MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity) {
+	@ModifyVariable(method = "renderFire", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/util/SpriteIdentifier;getSprite()Lnet/minecraft/client/texture/Sprite;", ordinal = 0), ordinal = 0)
+	private Sprite getSprite0(Sprite sprite, MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity) {
 		if (OnSoulFireComponent.get(entity).isOnSoulFire()) {
 			return SOUL_FIRE_0.getSprite();
 		}
-		return spriteIdentifier.getSprite();
+		return sprite;
 	}
 	
-	@Redirect(method = "renderFire", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/SpriteIdentifier;getSprite()Lnet/minecraft/client/texture/Sprite;", ordinal = 1))
-	private Sprite getSprite1(SpriteIdentifier spriteIdentifier, MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity) {
+	@ModifyVariable(method = "renderFire", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/util/SpriteIdentifier;getSprite()Lnet/minecraft/client/texture/Sprite;", ordinal = 1), ordinal = 1)
+	private Sprite getSprite1(Sprite sprite, MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity) {
 		if (OnSoulFireComponent.get(entity).isOnSoulFire()) {
 			return SOUL_FIRE_1.getSprite();
 		}
-		return spriteIdentifier.getSprite();
+		return sprite;
 	}
 }
--- src/main/java/moriyashiine/onsoulfire/mixin/client/InGameOverlayRendererMixin.java
+++ src/main/java/moriyashiine/onsoulfire/mixin/client/InGameOverlayRendererMixin.java
@@ -11,18 +11,18 @@ import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Environment(EnvType.CLIENT)
 @Mixin(InGameOverlayRenderer.class)
 public class InGameOverlayRendererMixin {
 	private static final SpriteIdentifier SOUL_FIRE_1 = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, new Identifier("block/soul_fire_1"));
 	
-	@Redirect(method = "renderFireOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/SpriteIdentifier;getSprite()Lnet/minecraft/client/texture/Sprite;"))
-	private static Sprite getSprite(SpriteIdentifier spriteIdentifier, MinecraftClient minecraftClient) {
+	@ModifyVariable(method = "renderFireOverlay", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/util/SpriteIdentifier;getSprite()Lnet/minecraft/client/texture/Sprite;"))
+	private static Sprite getSprite(Sprite sprite, MinecraftClient minecraftClient) {
 		if (OnSoulFireComponent.get(minecraftClient.player).isOnSoulFire()) {
 			return SOUL_FIRE_1.getSprite();
 		}
-		return spriteIdentifier.getSprite();
+		return sprite;
 	}
 }
