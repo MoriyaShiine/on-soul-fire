028ac7a8c44dc5bb6f2968ccdee696790f651a26
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-05-28 13:38:37 -0500

    fix #13

---
+++ src/main/java/moriyashiine/onsoulfire/mixin/EntityMixin.java
@@ -0,0 +1,20 @@
+package moriyashiine.onsoulfire.mixin;
+
+import moriyashiine.onsoulfire.common.component.entity.OnSoulFireComponent;
+import moriyashiine.onsoulfire.common.init.ModEntityComponents;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+	@Inject(method = "setOnFireFromLava", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;setOnFireFor(I)V"))
+	private void onsoulfire$setOnSoulFire(CallbackInfo ci) {
+		OnSoulFireComponent onSoulFireComponent = ModEntityComponents.ON_SOUL_FIRE.get(this);
+		if (onSoulFireComponent.isOnSoulFire()) {
+			onSoulFireComponent.setOnSoulFire(false);
+		}
+	}
+}
