ee26c63d54b400fcca1086a2d5b3f653384cf21a
CommandGenius <83155590+CommandGenius@users.noreply.github.com>
2024-06-14 01:43:37 -0400

    Update OnSoulFire.java

--- src/main/java/moriyashiine/onsoulfire/common/OnSoulFire.java
+++ src/main/java/moriyashiine/onsoulfire/common/OnSoulFire.java
@@ -16,6 +16,6 @@ public class OnSoulFire implements ModInitializer {
 	}
 
 	public static Identifier id(String value) {
-		return new Identifier(MOD_ID, value);
+		return Identifier.of(MOD_ID, value);
 	}
 }
