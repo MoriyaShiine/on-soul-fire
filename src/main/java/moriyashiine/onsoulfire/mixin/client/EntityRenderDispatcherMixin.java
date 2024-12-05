a1948358ed987282ccb8248eab63d64053be5239
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-12-05 13:00:40 -0600

    update to 1.21.4

--- src/main/java/moriyashiine/onsoulfire/mixin/client/EntityRenderDispatcherMixin.java
+++ src/main/java/moriyashiine/onsoulfire/mixin/client/EntityRenderDispatcherMixin.java
@@ -13,10 +13,10 @@ import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.state.EntityRenderState;
 import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -28,9 +28,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EntityRenderDispatcher.class)
 public class EntityRenderDispatcherMixin {
 	@Unique
-	private static final SpriteIdentifier SOUL_FIRE_0 = new SpriteIdentifier(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE, Identifier.ofVanilla("block/soul_fire_0"));
+	private static final SpriteIdentifier SOUL_FIRE_0 = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, Identifier.ofVanilla("block/soul_fire_0"));
 	@Unique
-	private static final SpriteIdentifier SOUL_FIRE_1 = new SpriteIdentifier(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE, Identifier.ofVanilla("block/soul_fire_1"));
+	private static final SpriteIdentifier SOUL_FIRE_1 = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, Identifier.ofVanilla("block/soul_fire_1"));
 
 	@Unique
 	private static Entity cachedEntity = null;
--- src/main/java/moriyashiine/onsoulfire/mixin/client/InGameOverlayRendererMixin.java
+++ src/main/java/moriyashiine/onsoulfire/mixin/client/InGameOverlayRendererMixin.java
@@ -11,8 +11,8 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameOverlayRenderer;
 import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.util.SpriteIdentifier;
-import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -22,11 +22,11 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(InGameOverlayRenderer.class)
 public class InGameOverlayRendererMixin {
 	@Unique
-	private static final SpriteIdentifier SOUL_FIRE_1 = new SpriteIdentifier(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE, Identifier.ofVanilla("block/soul_fire_1"));
+	private static final SpriteIdentifier SOUL_FIRE_1 = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE, Identifier.ofVanilla("block/soul_fire_1"));
 
 	@WrapOperation(method = "renderFireOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/SpriteIdentifier;getSprite()Lnet/minecraft/client/texture/Sprite;"))
-	private static Sprite onsoulfire$renderSoulFire(SpriteIdentifier instance, Operation<Sprite> original, MinecraftClient client) {
-		if (ModEntityComponents.ON_SOUL_FIRE.get(client.getCameraEntity()).isOnSoulFire()) {
+	private static Sprite onsoulfire$renderSoulFire(SpriteIdentifier instance, Operation<Sprite> original) {
+		if (ModEntityComponents.ON_SOUL_FIRE.get(MinecraftClient.getInstance().getCameraEntity()).isOnSoulFire()) {
 			return SOUL_FIRE_1.getSprite();
 		}
 		return original.call(instance);
